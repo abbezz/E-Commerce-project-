ee52fd950d4375fe5215e281188530d9b997830d
Abdollah Zamannoun <abbezz@live.com>
2022-07-05 20:11:40 +0100

    server is created

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+
+const init = async () => {
+  try {
+    await sequelize.sync({ force: false });
+
+    app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
+  } catch (error) {
+    console.log(`Error: Failed to connect to database - ${error.message}`);
+  }
+};
+
+init();
